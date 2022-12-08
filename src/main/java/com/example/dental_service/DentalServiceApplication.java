a2025d1f68320d2ffcb4c77ee01c2052646789ec
@zuhriddin.musabayev.2002 <zuhriddin.musabayev.2002@gmail.com>
2022-12-08 11:58:32 +0500

    first commit

---
+++ src/main/java/com/example/dental_service/DentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dental_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dental_service/DentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dental_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
